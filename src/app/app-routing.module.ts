91f406a2c55c2c5fa3b715127926dee6c4b5d0e9
stevecyril45 <stevechibuikecyril@gmail.com>
2025-10-08 21:14:33 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/pages_section/blog/blog.component';
+import { TeamComponent } from './pages/pages_section/team/team.component';
+import { TestimonialComponent } from './pages/pages_section/testimonial/testimonial.component';
+import { FaqsComponent } from './pages/pages_section/faqs/faqs.component';
+import { Page404Component } from './pages/pages_section/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'project', component: ProjectComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'team', component: TeamComponent},
+  {path: 'testimonial', component: TestimonialComponent},
+  {path: 'faqs', component: FaqsComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/pages_section/blog/blog.component';
+import { TeamComponent } from './pages/pages_section/team/team.component';
+import { TestimonialComponent } from './pages/pages_section/testimonial/testimonial.component';
+import { FaqsComponent } from './pages/pages_section/faqs/faqs.component';
+import { Page404Component } from './pages/pages_section/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectComponent,
+    ContactComponent,
+    BlogComponent,
+    TeamComponent,
+    TestimonialComponent,
+    FaqsComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
