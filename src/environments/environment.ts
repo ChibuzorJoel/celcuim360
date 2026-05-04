d07392cef9306982cba6996181a73e1c2a1f188a
ChibuzorJoel <chibuzoronyedikachi72@gmail.com>
2026-05-04 07:42:58 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 // src/environments/environment.ts  (development)
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000'   // swap to your Node.js server address
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5000'   // swap to your Node.js server address
+// // };
 
 // src/environments/environment.prod.ts  (production)
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://api.celcium360solutions.com'
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.celcium360solutions.com'
+};
