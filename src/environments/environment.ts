fa31aba739126e690b1af488fddacd2302a57829
ChibuzorJoel <chibuzoronyedikachi72@gmail.com>
2026-05-13 12:01:48 +0100

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 // src/environments/environment.ts  (development)
 
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000'
-};
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://hydrant-roamer-frenzied.ngrok-free.dev'  // swap to your Node.js server address
+//   apiUrl: 'http://localhost:5000'
 // };
+export const environment = {
+  production: false,
+  apiUrl: 'https://hydrant-roamer-frenzied.ngrok-free.dev'  // swap to your Node.js server address
+};
 
 // src/environments/environment.prod.ts  (production)
 // export const environment = {
